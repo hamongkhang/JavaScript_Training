95e04f3dc2678f26480e4695bdb6c5f558c06991
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-10-14 22:38:41 +0700

    Exercise 2

---
+++ Exercise_2.js
@@ -0,0 +1,87 @@
+const inventors = [
+    "Albert Einstein",
+    "Issac Newton",
+    "Galileo Galilei",
+    "Marie Curie",
+    "Johannes Kepler",
+    "Nicolaus Copernicus",
+    "Max Planck",
+    "Katherine Blodgett",
+    "Ada Lovelace",
+    "Sarah E. Goode",
+    "Lise Meitner",
+    "Hanna Hammarstrom"
+  ];
+  
+  const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
+  ////////////////////////some/every/////////////////////
+  // Does any inventor have the letter 'y' in their name?
+
+  
+  // Does every inventor have the letter 'e' in their name?
+ 
+  
+  // Does every inventor have first name that's longer than 4 characters?
+
+  
+  /////////////////filter////////////////////////////////////
+  // Print out an array of the inventors whose name starts with 'A'.
+
+  
+  // Print out an array of the inventors whose name contains 'n'.
+
+  
+  // Print out an array of the inventors whose name has the same letter twice in a row (e.g. nn or mm).
+
+
+  // Print out an array of the numbers which are odd.
+
+  
+  // Print out an array of the numbers that have two digits.
+
+  
+  // Print out an array of the numbers which are prime.
+
+  
+  /////////////////MAP/////////////////////////
+  // Print out the first name of each inventor.
+
+
+  // Print out the length of every inventor's full name.
+
+  
+  // Print out initials of all inventors(e.g. A.E., I.N., ...)
+
+  
+  // Print out an array of every number multiplied by 100.
+
+  
+  ////////////////////find/findIndex///////////////////////
+  // Find the inventor that has a middle name.
+
+  
+  // Bonus: Return a new array, that only has inventors without a middle name. (Hint: think about splice, if you use findIndex. But you may also use another of the methods you've learned about above.)
+
+
+  // Find the number divisible by 7.
+
+  // task 1: Given a string, Write a function to reverse each word in the sentence
+
+
+// task 2: Write a recursive function to reverse the string.
+
+
+// task 3: Given an array, Write a function to reverse the items in that array.
+
+
+// task 4: write a recursive function that reverses an array
+
+
+// task 5: write a recursive sum function
+
+
+// task 6: Write a recursive function to know if a number is prime
+
+
+// task 7: Write a class that includes 4 methods: add, update, remove, compare
+
